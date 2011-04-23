448e43b226d2bfdff29ff6459d057f2ab77cf47c
rjeitani@gmail.com <rjeitani@gmail.com@99cfd586-60a6-d4e2-6579-a72ecfd68079>
2011-04-23 04:42:52 +0000

    Added sample input file. Now reads input file line by line.

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -1,10 +1,53 @@
 package scheduler;
 //import java.util.*;
+import java.io.*;
 
 
 public class Scheduler {
 	public static void main (String[] args){
 		System.out.println("CAD Final Project");
+		
+		System.out.println("First argument = " + args[0]);
+		
+		readFile(args[0]);
 	}
+	
+	
+	public static void readFile(String filename){
+		
+		System.out.println("In Readfile: " + filename);
+	try{
+			FileReader input = new FileReader(filename);
+			BufferedReader bufRead = new BufferedReader(input); 
+			String line = "";
+			int count = 0;
+			// read a line
+			try {
+				line = bufRead.readLine();
+			} catch (IOException e) {
+				System.out.println("Cannot Read File.");
+				e.printStackTrace();
+			}
+			count++;
+			
+			while (line != null)
+			{
+				System.out.println("Line Read: " + line);
+				System.out.println("The count is: " + count);
+				line = bufRead.readLine();
+				count++;
+			}
+			
+	} catch (FileNotFoundException e){
+			System.out.println("File Not Found!");
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+		
+	}
+	
+	
 
 }
